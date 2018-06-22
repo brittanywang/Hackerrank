ff0b8951403dc42fbd3fc15861132de2a31f603f
brittanywang <35832643+brittanywang@users.noreply.github.com>
2018-06-21 19:03:41 -0700

    detect a cycle

---
+++ DetectaCycle.java
@@ -0,0 +1,17 @@
+/*
+detect a cycle
+*/
+
+boolean hasCycle(Node head) {
+    Node fast = head;
+    
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        head = head.next;
+        
+        if(head.equals(fast)) {
+            return true;
+        }
+    }
+    return false;
+}
