cb40cc39741998570d923fe9f7a5c551d16a1773
Brittany <bw943600@gmail.com>
2018-07-02 21:55:15 -0700

    updated three files

---
+++ DiagonalDifference.java
@@ -0,0 +1,6 @@
+/*
+Input: the first line contains a single integer, n denoting the number of rows and columns in the matrix a.
+The next n lines denote the matrix a's rows, with each line contain n space-separated integers describing the columns.
+
+Output: Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+*/
